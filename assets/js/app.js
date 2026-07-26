4738502f8c0d16be777711db8ef7377d735fa0d6
GsebLearning <videoanime4k@gmail.com>
2026-07-26 21:27:28 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -169,3 +169,19 @@ sidebar.classList.remove("active");
 }
 
 });
+// Register Service Worker
+
+if ("serviceWorker" in navigator) {
+
+window.addEventListener("load",()=>{
+
+navigator.serviceWorker.register("sw.js")
+.then(()=>{
+
+console.log("Service Worker Registered");
+
+});
+
+});
+
+}
